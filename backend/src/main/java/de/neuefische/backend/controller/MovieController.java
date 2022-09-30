cb4413e6fc1ec69e1cb32b85b3899f95915b0064
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-30 11:45:54 +0200

    BE: add constructor for MovieController

--- backend/src/main/java/de/neuefische/backend/controller/MovieController.java
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -2,6 +2,7 @@ package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.Movie;
 import de.neuefische.backend.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,10 @@ import java.util.List;
 public class MovieController {
 
     private MovieService movieService;
-
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
     @GetMapping
     public List<Movie> getAllMovies(){
