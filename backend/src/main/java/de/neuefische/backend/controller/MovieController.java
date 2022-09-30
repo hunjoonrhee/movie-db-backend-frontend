743d75f76c6cbaeee00199490df5f7181650bb1f
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-30 11:53:29 +0200

    BE: Modify Annotation

--- backend/src/main/java/de/neuefische/backend/controller/MovieController.java
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -1,15 +1,17 @@
 package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.Movie;
+import de.neuefische.backend.model.MovieDTO;
 import de.neuefische.backend.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/movies")
 public class MovieController {
 
@@ -22,6 +24,11 @@ public class MovieController {
     @GetMapping
     public List<Movie> getAllMovies(){
         return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public Movie addMovie(MovieDTO newMovie){
+        return movieService.addMovie(newMovie);
     }
 
 }
